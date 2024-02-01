49d3e2a9af53c99b48b81f9a9b5386f78d4f2b64
jbolda2 <jbolda2@my.wgu.edu>
2024-02-01 15:51:57 -0500

    Bootstrap was updated to fill the parts repository and prodcuts repository if they are empty. A controller was made to map the about page url. A controller was made for the next task of making a buy now button. Finally the about page was updated to properly route back to the mainscreen.html page

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,5 +1,6 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
@@ -38,40 +39,69 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+    if (partRepository.count() == 0) {
+        OutsourcedPart counter = new OutsourcedPart();
+        counter.setCompanyName("Jim's Shoe Parts");
+        counter.setName("Counter");
+        counter.setInv(5);
+        counter.setPrice(14.0);
+        counter.setId(100L);
+        outsourcedPartRepository.save(counter);
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+        OutsourcedPart sole = new OutsourcedPart();
+        sole.setCompanyName("Gentlemen's Footwear");
+        sole.setName("Sole");
+        sole.setInv(5);
+        sole.setPrice(15.0);
+        sole.setId(101L);
+        outsourcedPartRepository.save(sole);
+
+
+        InhousePart tongue = new InhousePart();
+        tongue.setPartId(52);
+        tongue.setName("Tongue");
+        tongue.setInv(5);
+        tongue.setPrice(16.0);
+        tongue.setId(102L);
+        partRepository.save(tongue);
+
+        InhousePart laces = new InhousePart();
+        laces.setPartId(53);
+        laces.setName("Laces");
+        laces.setInv(5);
+        laces.setPrice(17.0);
+        laces.setId(103L);
+        partRepository.save(laces);
+
+        OutsourcedPart toecap = new OutsourcedPart();
+        toecap.setCompanyName("Jim's Shoe Parts");
+        toecap.setName("Toecap");
+        toecap.setInv(5);
+        toecap.setPrice(18.0);
+        toecap.setId(104L);
+        outsourcedPartRepository.save(toecap);
+    }
+
+
+        if (productRepository.count() == 0) {
+            Product oxford = new Product("Oxford", 80.0, 15);
+            productRepository.save(oxford);
+            Product loafer = new Product("Loafer", 85.0, 15);
+            productRepository.save(loafer);
+            Product derby = new Product("Derby", 90.0, 15);
+            productRepository.save(derby);
+            Product brogue = new Product("Brogue", 95.0, 15);
+            productRepository.save(brogue);
+            Product wingTip = new Product("Wing Tip", 100.0, 15);
+            productRepository.save(wingTip);
         }
 
-        System.out.println(thePart.getCompanyName());
-        */
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            System.out.println(part.getName()+" "+part.getCompanyName());
-        }
-
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
-
-        System.out.println("Started in Bootstrap");
+        /*System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
         System.out.println(productRepository.findAll());
         System.out.println("Number of Parts"+partRepository.count());
         System.out.println(partRepository.findAll());
+*/
 
     }
 }
